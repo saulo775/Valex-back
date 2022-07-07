2166165e05213c9c27b2468c9c0fd12ba12b4337
saulo775 <59702227+saulo775@users.noreply.github.com>
2022-07-06 22:48:59 -0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+app.listen(5500, ()=>{
+    console.log("Server running on port 5500!");
+});
