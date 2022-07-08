a80e5b34ca7336877d043c474008674e3208f7de
saulo775 <59702227+saulo775@users.noreply.github.com>
2022-07-08 10:34:11 -0300

    FEAT - Conexão e criação do banco de dados

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import express from "express";
+import express, { json } from "express";
 import cors from "cors";
 
 const app = express();
-
+app.use(json());
 app.use(cors());
 
-app.listen(5500, ()=>{
-    console.log("Server running on port 5500!");
+
+const PORT = +process.env.PORT || 5500;
+app.listen(PORT , ()=>{
+    console.log(`Server running on port ${PORT}...`);
 });
