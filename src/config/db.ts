f79543c0df037721a9c5861af2d29145d39242a5
saulo775 <59702227+saulo775@users.noreply.github.com>
2022-07-12 21:30:24 -0300

    Deploy configs

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,15 +4,15 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const { Pool } = pg;
-
 const configDatabase = {
     connectionString: process.env.DATABASE_URL
 };
 
-// if (process.env.MODE === "PROD") {
-//     configDatabase.ssl = {
-//         rejectUnauthorized: false
-//     }
-// }
+if (process.env.MODE === "PROD") {
+    configDatabase.ssl = {
+        rejectUnauthorized: false
+    }
+}
+
 
 export const connection = new Pool(configDatabase);
